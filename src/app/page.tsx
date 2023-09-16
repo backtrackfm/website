5b68a7ce533d0f477e825c533a5f1638561ea6f0
tika <48658947+tika@users.noreply.github.com>
2023-09-16 20:28:31 +0100

    feat: update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { AppWindow, Apple, GithubIcon } from "lucide-react";
+import { AppWindow, Apple } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="bg-gradient-to-b from-primary to-yellow-500 w-3/5 flex justify-center items-center h-32 rounded-lg">
-        <div className="flex gap-4 items-center ">
+    <main className="flex min-h-screen flex-col items-center justify-between p-6 md:p-24 bg-white text-black">
+      <div className="bg-gradient-to-b from-primary to-yellow-500 w-full md:w-3/5 flex justify-center items-center h-32 rounded-lg">
+        <div className="flex gap-4 items-center text-white">
           <Image src="/logo.svg" alt="⏮️" width={60} height={60} />
           <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
             Backtrack
@@ -16,7 +16,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-3/5 text-justify">
+      <div className="w-full md:w-3/5 text-justify">
         Backtrack solves a simple problem that I faced whilst producing some
         music: it&apos;s really hard to keep track of the latest version of a
         song. Backtrack solves this problem by providing the user a simple yet
@@ -25,9 +25,9 @@ export default function Home() {
         Find out more{" "}
         <Link
           href="https://github.com/tika/backtrack-app#backtrack"
-          className="underline"
+          className="underline hover:text-yellow-500"
         >
-          here
+          on the open-source GitHub
         </Link>
         .
       </div>
@@ -50,17 +50,14 @@ export default function Home() {
 
       <div className="text-center">
         <p className="leading-7 text-sm [&:not(:first-child)]:mt-6">
-          Created & thought out by{" "}
-          <Link href="https://github.com/tika" className="underline">
+          A thought & creation of{" "}
+          <Link
+            href="https://github.com/tika"
+            className="underline hover:text-yellow-500"
+          >
             Tika Capon
           </Link>
           <br />
-          <Link
-            href="https://github.com/tika/backtrack-app"
-            className={cn("flex gap-2", buttonVariants({ variant: "link" }))}
-          >
-            <GithubIcon /> Find me on GitHub
-          </Link>
         </p>
       </div>
     </main>
